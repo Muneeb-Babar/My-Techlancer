a28706e719706d6e13f34ddaa276d9836676b46a
muneeb-babar <muneeb63ahmad63@gmail.com>
2024-09-18 19:38:23 +0500

    fix UI

--- src/Pages/PricingPage.jsx
+++ src/Pages/PricingPage.jsx
@@ -1,4 +1,4 @@
-import Blog1 from '../Components/Blog/Blog1';
+// import Blog1 from '../Components/Blog/Blog1';
 import BreadCumb from '../Components/Common/BreadCumb';
 import Pricing from '../Components/Pricing/Pricing';
 
@@ -7,7 +7,7 @@ const PricingPage = () => {
         <div className='pricing-page'>
             <BreadCumb Title="Pricing"></BreadCumb> 
             <Pricing></Pricing>
-            <Blog1></Blog1>
+            {/* <Blog1></Blog1> */}
         </div>
     );
 };
