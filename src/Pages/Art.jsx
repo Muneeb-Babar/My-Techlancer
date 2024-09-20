b84934e330042595dbb78709201df90413b0f3e5
muneeb-babar <muneeb63ahmad63@gmail.com>
2024-09-20 19:52:32 +0500

    art page completed

--- src/Pages/Art.jsx
+++ src/Pages/Art.jsx
@@ -1,39 +1,47 @@
 import React from 'react'
 import CardPage from "../Components/CardsPage/CardsPage";
 
+import character1 from '../assets/images/character-modeling.avif'
+import character2 from '../assets/images/projects/2D/art5.jpg'
+import character3 from '../assets/images/character-modeling2.avif'
+
+import icon1 from '../assets/images/stacks/art/icon1.png'
+import icon2 from '../assets/images/stacks/art/icon2.png'
+import icon3 from '../assets/images/stacks/art/icon3.png'
+import icon4 from '../assets/images/stacks/art/icon4.png'
+
 const Art = () => {
-  // const techStacks = [
-  //   { name: "SEO", src: Seo },
-  //   { name: "Analysis", src: Analysis },
-  //   { name: "Emal", src: Email },
-  //   { name: "Affiliate", src: Affiliate },
-  //   { name: "HubSpot", src: Hubspot },
-  // ];
+  const techStacks = [
+    { name: "SEO", src: icon1 },
+    { name: "Analysis", src: icon2 },
+    { name: "Emal", src: icon3 },
+    { name: "Affiliate", src: icon4 }
+  ];
 
   const Comp1 = {
-    img1: "https://img.freepik.com/free-photo/social-media-marketing-concept-marketing-with-applications_23-2150063134.jpg?t=st=1726766185~exp=1726769785~hmac=66b1f58baef42cef8324273e625b06f61a995220eb1cc53e2a2128842d65d333&w=900",
-    heading1: "Digital Marketing",
-    para1: `Digital marketing refers to the strategic use of digital channels, platforms, and technologies to promote products, services, or brands to a targeted audience. It encompasses a vast array of online marketing strategies and tactics aimed at engaging potential customers, generating leads, and driving conversions. By leveraging the power of the internet and digital tools, businesses can connect with their audience where they spend much of their time—online. Digital marketing has become an essential aspect of modern business operations due to its ability to reach a global audience with precision and efficiency.`,
+    img1: character1,
+    heading1: "Character Modeling",
+    para1: `Character modeling is the art and science of creating the visual representation of characters for various media, including animation, video games, and illustrations. This multifaceted process involves conceptualizing and designing characters, whether in 2D or 3D formats, to convey their personality, traits, and stories effectively. In 2D character modeling, artists focus on flat designs that utilize color, line, and shape to bring characters to life on a two-dimensional plane. Conversely, 3D character modeling involves creating detailed, lifelike representations that can move and interact within a three-dimensional space. Both techniques require a blend of artistic creativity and technical skill, enabling creators to engage audiences and enhance storytelling through captivating character designs.`,
   };
 
   const Comp2 = {
-    img2: "https://img.freepik.com/premium-photo/affiliate-marketing-milestones-concept-business-concept-text_384948-9984.jpg?w=900",
-    heading2: "Affiliate Marketing",
-    para2: `Affiliate marketing is a strategic, performance-based marketing model where businesses collaborate with external partners, known as affiliates, to promote their products or services. These affiliates can include influencers, bloggers, websites, or even specialized affiliate marketers who have established audiences and platforms. The primary goal of affiliate marketing is to leverage the affiliate’s reach to expand the business’s visibility, generate leads, and ultimately drive sales. Affiliates earn a commission based on the sales or conversions they help generate, making it a cost-effective, results-driven model.`,
+    img2: character2,
+    heading2: "2D Characters",
+    para2: `2D character modeling is a creative process that involves designing and illustrating characters in a flat, two-dimensional space. This technique is widely used in animation, video games, and illustrations, where artists bring characters to life through vibrant colors, distinct shapes, and expressive features. By focusing on line art and flat designs, 2D character modeling allows for a unique visual style that captures the essence of characters in a way that resonates with audiences. Whether for cartoons, mobile games, or digital illustrations, 2D character modeling emphasizes artistic flair and storytelling through dynamic poses and engaging visuals.`,
   };
 
   const Comp3 = {
-    img3: "https://img.freepik.com/free-photo/e-mail-global-communications-connection-social-networking-concept_53876-123795.jpg?t=st=1726766426~exp=1726770026~hmac=650ba77802948b61f4e9b2c2a2fa8f43c60336adc4b6082ab6bd6da84b028838&w=900",
-    heading3: "Email Marketing",
-    para3: `Email marketing remains one of the most powerful and cost-effective forms of direct communication with customers. It enables businesses to connect with their audience on a personal level, delivering tailored messages straight to their inboxes. Through personalized and targeted email campaigns, businesses can build stronger relationships with both potential and existing customers, ensuring that they stay engaged with the brand over time.`,
+    img3: character3,
+    heading3: "3D Characters",
+    para3: `3D character modeling is an essential component of modern animation and game design, where artists create lifelike, three-dimensional representations of characters. This process involves sculpting, texturing, and rigging models to ensure they can move and express emotions realistically within a virtual environment. Using advanced software, creators can add depth and detail to characters, from their physical features to intricate clothing designs. 3D character modeling not only enhances visual storytelling but also allows for immersive experiences in films, games, and virtual reality, where audiences can interact with characters in a believable and engaging way.`,
   };
   return (
     <CardPage
-      // tectStacks={techStacks}
+      tectStacks={techStacks}
       Comp1={Comp1}
       Comp2={Comp2}
       Comp3={Comp3}
-      pageName={"2D/3D Characters"}
+      pageName={"2D/3D Character Modeling"}
     />
   );
 };
